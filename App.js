9e05019629dbf430ce6a48d639bd1bd1ab2182d8
GeorgeP225 <gpayne@greeningaustralia.org.au>
2022-01-21 01:50:22 +1000

    Inital template- meals app

--- App.js
+++ App.js
@@ -1,70 +1,10 @@
-import React, {useState} from 'react'
-import { 
-  StyleSheet,
-  View
-} from 'react-native'
-import { SafeAreaView } from 'react-native-web'
-import Header from './components/Header'
-import StartGameScreen from './screens/StartGameScreen'
-import GameScreen from './screens/GameScreen'
-import GameOverScreen from './screens/GameOverScreen'
-import * as Font from 'expo-font'
-//import { AppLoading } from 'expo'
-//expo install expo-app-loading
-import AppLoading from 'expo-app-loading'
+import React from 'react'
+import { Text, View } from 'react-native'
 
-//React 16.8 is the release of functional components
-
-//app.json:     "orientation": "portrait", "landscape", "default"
-
-const fetchFonts = () => {
-  Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  })
-}
-
-export default function App() {
-  const [userNumber, setUserNumber] = useState()
-  const [guessRounds, setGuessRounds] = useState(0)
-  const [dataLoaded, setDataLoaded] = useState(false)
-  if (!dataLoaded) {
-    return ( 
-      <AppLoading 
-        startAsync={fetchFonts} 
-        onFinish={() => setDataLoaded(true)} 
-        onError={(error) => console.log(error)}
-        />
-    )
-  }
-  const configureNewGameHandler = () => {
-    setGuessRounds(0)
-    setUserNumber(null)
-  }
-  const startGameHandler = selectedNumber => {
-    setUserNumber(selectedNumber)
-    setGuessRounds(0)
-  }
-  const gameOverHandler = numOfRounds => {
-    setGuessRounds(numOfRounds)
-  }
-  let content = <StartGameScreen onStartGame={startGameHandler} />
-  if (userNumber && guessRounds <= 0) {
-    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-  } else if (guessRounds > 0) {
-    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />
-  }
+export default App = () => {
   return (
-    <SafeAreaView style={styles.screen}>
-      <Header title='Guess a Number' />
-      {/* <StartGameScreen onStartGame={startGameHandler} /> */}
-      {content}
-    </SafeAreaView>
+    <View>
+      <Text>Open to edit.</Text>
+    </View>
   )
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1
-  }
-})
