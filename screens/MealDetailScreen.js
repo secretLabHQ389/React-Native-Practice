b9d07f1ab28aa1a18065f14cfa26afcf713f51d0
GeorgeP225 <gpayne@greeningaustralia.org.au>
2022-01-29 18:41:05 +1000

    Meal Detail Screen refactor

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -3,23 +3,53 @@ import {
   View,
   Text,
   Button,
+  Image,
   StyleSheet
 } from 'react-native'
+import { ScrollView } from 'react-native-web'
 import { HeaderButton } from 'react-navigation-header-buttons'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { MEALS } from '../data/dummy-data'
+import DefaultText from '../components/DefaultText'
+
+const ListItem = props => {
+  const {children} = props
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{children}</DefaultText>
+    </View>
+  )
+}
 
 const MealDetailScreen = props => {
   const {navigation} = props
   const mealId = navigation.getParam('mealId')
   const selectedMeal = MEALS.find(meal => meal.id === mealId)
+  console.log('selectedMeal: ', selectedMeal)
   return (
-    <View>
-      <Text>{selectedMeal.title}</Text>
-      <Button title='Go Back to Categories' onPress={() => {
-          navigation.popToTop() //goes back to root screen
-        }} />
-    </View>
+    <ScrollView>
+      <Image 
+        style={styles.image} 
+        source={{uri: selectedMeal.imageUrl}} 
+        />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map(ingredient => {
+        return (
+          <ListItem key={ingredient}>{ingredient}</ListItem>
+        )
+      })}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map(step => {
+        return (
+          <ListItem key={step}>{step}</ListItem>
+        )
+      })}
+    </ScrollView>
   )
 }
 
@@ -41,10 +71,26 @@ MealDetailScreen.navigationOptions = navigationData => {
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  image: {
+    width: '100%',
+    height: 200
+  },
+  details: {
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center'
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1
   }
 })
 
