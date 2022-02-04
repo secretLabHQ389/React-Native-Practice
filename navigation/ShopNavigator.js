c1048156d798f15e7d348b7b8cbcada578f2ccaf
GeorgeP225 <gpayne@greeningaustralia.org.au>
2022-02-04 12:17:34 +1000

    Redux action in multiple reducers

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import { createDrawerNavigator } from 'react-navigation-drawer'
@@ -5,10 +6,11 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
 import Colors from '../constants/Colors'
 import { 
   Platform 
-} from 'react-native'
+} from 'react-native-web'
 import ProductDetailScreen from '../screens/shop/ProductDetailsScreen'
 import CartScreen from '../screens/shop/CartScreen'
 import OrdersScreen from '../screens/shop/OrdersScreen'
+import { Ionicons } from '@expo/vector-icons'
 
 const defaultNavOptions = {
   headerStyle: {
@@ -30,12 +32,30 @@ const ProductsNavigator = createStackNavigator({
   ProductDetail: ProductDetailScreen,
   Cart: CartScreen
 }, {
+  navigationOptions: {
+    drawerIcon: drawerConfig => (
+      <Ionicons 
+        name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+        size={23}
+        color={drawerConfig.tintColor}
+        />
+    )
+  },
   defaultNavigationOptions: defaultNavOptions
 })
 
 const OrdersNavigator = createStackNavigator({
   Orders: OrdersScreen
 }, {
+  navigationOptions: {
+    drawerIcon: drawerConfig => (
+      <Ionicons 
+        name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+        size={23}
+        color={drawerConfig.tintColor}
+        />
+    )
+  },
   defaultNavigationOptions: defaultNavOptions
 })
 
