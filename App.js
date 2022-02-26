07c6be119ca57393dbfcc31ce484f4f891fafc43
GeorgeP225 <gpayne@greeningaustralia.org.au>
2022-02-26 18:59:46 +1000

    Utilities template

--- App.js
+++ App.js
@@ -1,48 +1,12 @@
 import React, {useState} from 'react'
-import * as Font from 'expo-font'
-import AppLoading from 'expo-app-loading'
-//import { useScreens } from 'react-native-screens'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import productsReducer from './store/reducers/products'
-import cartReducer from './store/reducers/cart'
-import ordersReducer from './store/reducers/orders'
-import authReducer from './store/reducers/auth'
-import NavigationContainer from './navigation/NavigationContainer'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import ReduxThunk from 'redux-thunk'
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  })
-}
-
-const rootReducer = combineReducers({
-  products: productsReducer,
-  cart: cartReducer,
-  orders: ordersReducer,
-  auth: authReducer
-})
-
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk/*, composeWithDevTools()*/))
+import {
+  View
+} from 'react-native'
 
 const App = () => {
-  const [fontLoaded, setFontLoaded] = useState(false)
-  if (!fontLoaded) {
-    return (
-      <AppLoading 
-        startAsync={fetchFonts} 
-        onFinish={() => setFontLoaded(true)} 
-        onError={err => console.log(err)}
-        />
-    )
-  }
+
   return (
-    <Provider store={store}>
-      <NavigationContainer />
-    </Provider>
+    <View></View>
   )
 }
 
